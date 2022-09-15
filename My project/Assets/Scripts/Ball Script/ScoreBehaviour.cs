8d6d49e2907f216b1f221016c4a24655b11c2faa
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-09-15 04:54:17 +0200

    preaper work fo score system

---
+++ My project/Assets/Scripts/Ball Script/ScoreBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player, computer;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
